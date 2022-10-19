c4ef44b01fcc9cdc001dc16a00dc05b4d9be743c
CatsInPartyPants <81612454+CatsInPartyPants@users.noreply.github.com>
2022-10-19 16:54:30 +0300

    +task 3

--- HW-dynArrAndStructures/HW-dynArrAndStructures.cpp
+++ HW-dynArrAndStructures/HW-dynArrAndStructures.cpp
@@ -7,6 +7,12 @@ int** addColumn(int** arr, int rows, int columns, int* arr2, int position);
 //task2
 int** deleteColumn(int** arr, int rows, int columns, int position);
 
+//task 3
+
+void moveRow(int** arr, int M, int N, int count);
+
+void moveColomn(int** arr, int M, int N, int count);
+
 //task4
 int** transponseMatrix(int** arr, int rows, int columns);
 
@@ -279,7 +285,48 @@ int main()
     }
     delete[] task2array;
 
-    
+    //task 3
+
+    int** arr3 = new int* [5];
+    for (int i = 0; i < 5; i++)
+    {
+        arr3[i] = new int[5];
+    }
+    srand(time(NULL));
+
+    // before 
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            arr3[i][j] = 1 + rand() % (50 + 1);
+            std::cout << arr3[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
+    //after
+    moveRow(arr3, 5, 5, 2);
+    moveColomn(arr3, 5, 5, 2);
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            std::cout << arr3[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    //delete dynamic array
+
+    for (int i = 0; i < 5; i++)
+    {
+        delete[] arr3[i];
+    }
+    delete[] arr3;
+
+
     //task 4
     std::cout << "\n\n\nTask4\n";
     //making matrix 5x5
@@ -453,6 +500,46 @@ int** deleteColumn(int** arr, int rows, int columns, int position)
     }
     delete[] arr;
     return new_arr;
+}
+
+//task 3
+
+// moving rows
+void moveRow(int** arr, int M, int N, int count)
+{
+
+    for (int i = 0; i < M; i++)
+    {
+        for (int k = 0; k < count; k++)
+        {
+            int temp;
+            temp = arr[i][0];
+            for (int j = 0; j < N; j++)
+            {
+                arr[i][j] = arr[i][j + 1];
+            }
+            arr[i][N - 1] = temp;
+        }
+    }
+}
+
+//moving colomns
+void moveColomn(int** arr, int M, int N, int count)
+{
+    for (int j = 0; j < N; j++)
+    {
+        for (int k = 0; k < count; k++)
+        {
+            int temp = 0;
+            temp = arr[0][j];
+            for (int i = 0; i < M - 1; i++)
+            {
+                arr[i][j] = arr[i + 1][j];
+            }
+            arr[M - 1][j] = temp;
+        }
+
+    }
 }
 
 //task 4

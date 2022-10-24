11a51385ba8270aac9d4e78f12cf978f779a6823
Artem Nilov <nilov1235@mail.ru>
2022-10-24 11:14:00 +0300

    + library

--- HW-dynArrAndStructures/HW-dynArrAndStructures.cpp
+++ HW-dynArrAndStructures/HW-dynArrAndStructures.cpp
@@ -460,7 +460,55 @@ int main()
     setlocale(LC_ALL, "ru");
     Book* book_info = new Book[10];
 
+    book_info[0].name = "Колобок";
+    book_info[0].author = "Писахов";
+    book_info[0].publisher = "Дрофа";
+    book_info[0].genre = "Фольклор";
 
+    book_info[1].name = "Сказки бабушки Галины";
+    book_info[1].author = "Галина";
+    book_info[1].publisher = "Дрофа";
+    book_info[1].genre = "Фольклор";
+
+    book_info[2].name = "Война и мир";
+    book_info[2].author = "Толстой";
+    book_info[2].publisher = "Просвещение";
+    book_info[2].genre = "Роман";
+
+    book_info[3].name = "Мастер и Маргарита";
+    book_info[3].author = "Булгаков";
+    book_info[3].publisher = "Проба";
+    book_info[3].genre = "Фэнтези";
+
+    book_info[4].name = "Двенадцать стульев";
+    book_info[4].author = "Петров";
+    book_info[4].publisher = "Проба";
+    book_info[4].genre = "Комедия";
+
+    book_info[5].name = "Преступление и наказание";
+    book_info[5].author = "Достоевский";
+    book_info[5].publisher = "Проба";
+    book_info[5].genre = "Детектив";
+
+    book_info[6].name = "Палата № 6";
+    book_info[6].author = "Чехов";
+    book_info[6].publisher = "Дрофа";
+    book_info[6].genre = "Комедия";
+
+    book_info[7].name = "Евгений Онегин";
+    book_info[7].author = "Пушкин";
+    book_info[7].publisher = "Проба";
+    book_info[7].genre = "Драма";
+
+    book_info[8].name = "Ревизор";
+    book_info[8].author = "Гоголь";
+    book_info[8].publisher = "Просвещение";
+    book_info[8].genre = "Комедия";
+
+    book_info[9].name = "Шерлок холмс";
+    book_info[9].author = "Доиль";
+    book_info[9].publisher = "Просвещение";
+    book_info[9].genre = "Детектив";
 
     show_all_books(book_info, 10);
     //edit_book(book_info, 10);

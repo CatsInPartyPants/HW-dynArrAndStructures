3768b0e9e84c6d0f8a0f08a38fe1541968260c66
Artem Nilov <nilov1235@mail.ru>
2022-10-24 11:13:10 +0300

    +library

--- HW-dynArrAndStructures/HW-dynArrAndStructures.cpp
+++ HW-dynArrAndStructures/HW-dynArrAndStructures.cpp
@@ -116,6 +116,29 @@ void show_car(Car* car)
     std::cout << "The gearbox type of the car is " << car->gearbox_type << std::endl;
 }
 
+//task3 struct (Library)
+
+struct Book {
+    std::string name;
+    std::string author;
+    std::string publisher;
+    std::string genre;
+};
+
+void show_all_books(Book* books, int size);
+
+int edit_book(Book* books, int size);
+
+void find_by_author(Book* books, std::string author, int size);
+
+void find_by_name(Book* books, std::string name, int size);
+
+void sort_by_author(Book* books, int size);
+
+void sort_by_publisher(Book* books, int size);
+
+void sort_by_name(Book* books, int size);
+
 //task4 struct
 
 struct Car_4 {
@@ -431,6 +454,23 @@ int main()
     Car_4* my_car;
     my_car = find_car_by_number(array_of_cars, 4899);
     std::cout << my_car->model;
+
+    //task 3 struct (Library)
+
+    setlocale(LC_ALL, "ru");
+    Book* book_info = new Book[10];
+
+
+
+    show_all_books(book_info, 10);
+    //edit_book(book_info, 10);
+
+    sort_by_name(book_info, 10);
+    show_all_books(book_info, 10);
+    sort_by_author(book_info, 10);
+    show_all_books(book_info, 10);
+
+    delete[] book_info;
 }
 
 //task 1
@@ -583,5 +623,124 @@ phone_name* find_by_name(phone_name* arr, std::string name)
     {
         if (arr[i].name == name)
             return arr + i;
+    }
+}
+
+//task 3 struct (Library)
+
+
+void show_all_books(Book* books, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << "Books in library: \n";
+        std::cout << i + 1 << "." << books[i].name << ",\n";
+        std::cout << books[i].author << ",\n";
+        std::cout << books[i].publisher << ",\n";
+        std::cout << books[i].genre << ",\n";
+    }
+    std::cout << std::endl;
+}
+
+int edit_book(Book* books, int size)
+{
+    int position;
+    std::cout << "What book do you want to edit?";
+    std::cin >> position;
+    if (position >= size)
+    {
+        return 1;
+    }
+    std::cin.get();
+    std::cout << "Enter the name of the book:";
+    std::getline(std::cin, books[position].name);
+
+    std::cout << "Enter the author of the book:";
+    std::getline(std::cin, books[position].author);
+
+    std::cout << "Enter the publisher of the book:";
+    std::getline(std::cin, books[position].publisher);
+
+    std::cout << "Enter the genre of the book:";
+    std::getline(std::cin, books[position].genre);
+    return 0;
+}
+
+void find_by_author(Book* books, std::string author, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (author == books[i].author)
+        {
+            std::cout << books[i].author << std::endl;
+            std::cout << books[i].name << std::endl;
+            std::cout << books[i].publisher << std::endl;
+            std::cout << books[i].genre << std::endl;
+        }
+    }
+}
+
+void find_by_name(Book* books, std::string name, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (name == books[i].name)
+        {
+            std::cout << books[i].author << std::endl;
+            std::cout << books[i].name << std::endl;
+            std::cout << books[i].publisher << std::endl;
+            std::cout << books[i].genre << std::endl;
+        }
+    }
+}
+
+void sort_by_author(Book* books, int size)
+{
+    Book temp;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (strcmp(books[i].author.c_str(), books[j].author.c_str()) == 1)
+            {
+                temp = books[i];
+                books[i] = books[j];
+                books[j] = temp;
+            }
+        }
+    }
+}
+
+void sort_by_name(Book* books, int size)
+{
+    Book temp;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (strcmp(books[i].name.c_str(), books[j].name.c_str()) == 1)
+            {
+                temp = books[i];
+                books[i] = books[j];
+                books[j] = temp;
+            }
+        }
+    }
+}
+
+void sort_by_publisher(Book* books, int size)
+{
+    Book temp;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (strcmp(books[i].publisher.c_str(), books[j].publisher.c_str()) == 1)
+            {
+                temp = books[i];
+                books[i] = books[j];
+                books[j] = temp;
+            }
+        }
     }
 }
